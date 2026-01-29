5d9ae1369d92c3ab5ba22ed82535ff48f3a22d9a
idsinternship <ids.internship@outlook.com>
2026-01-29 13:25:55 +0200

    5

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./styles/global.css";
+import { AuthProvider } from "./src/context/AuthContext";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
